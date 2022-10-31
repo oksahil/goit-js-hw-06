e6f0829416e4e2240decd9886be04519dc86f8c1
oksahil <oksana.khylyk@gmail.com>
2022-10-31 17:02:19 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  colorText: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+}
+
+refs.btn.addEventListener('click', btnClick);
+
+function btnClick() {
+  const colorClick = getRandomHexColor();
+  console.log(colorClick);
+  refs.colorText.textContent = colorClick;
+  refs.body.style.backgroundColor = colorClick;
+// refs.colorText.style.color = colorClick;
+};
