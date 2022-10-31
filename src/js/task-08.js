6560e356b6500fc2c65b1eb263ad43982269f865
oksahil <oksana.khylyk@gmail.com>
2022-10-31 15:18:14 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+
+  if (email === "" || password === "") {
+      return console.log("Please fill in all the fields!");
+  }
+    const formData = {
+        email,
+        password,
+    };
+
+  console.log(formData);
+  console.log(`Email: ${email}, Password: ${password}`);
+  event.currentTarget.reset();
+}
